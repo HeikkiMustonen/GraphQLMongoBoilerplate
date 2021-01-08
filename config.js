a4abb0e123157911f1d0934ca513084e730aac67
Heikki Mustonen <hmustone18@student.oulu.fi>
2021-01-08 16:07:06 +0200

    test

--- config.js
+++
@@ -1,6 +0,0 @@
-var config = {};
-
-config.expressListeningPort = 4000;
-config.mongoConnectionString = 'mongodb://Heikki:Balance2020@192.168.1.5:27017/?authSource=admin&authMechanism=SCRAM-SHA-256&readPreference=primary&appname=MongoDB%20Compass&ssl=false';
-
-module.exports = config;
